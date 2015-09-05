c8f8f44ed72a4f601888e1d6501a8dfed7edfe2e
Daniel Fowler <me@danfowler.net>
2015-09-05 15:13:20 +0300

    Adds ForeignKey support per #2
    When a given column in the JTS has a foreign key defined, it will add a
    reference to that other table/column (FK’s resource/fields).

--- jtssql/table.py
+++ jtssql/table.py
@@ -1,7 +1,7 @@
 import json
 
 from sqlalchemy import MetaData
-from sqlalchemy.schema import Table, Column
+from sqlalchemy.schema import Table, Column, ForeignKey
 from sqlalchemy.types import Unicode
 
 from jtssql.util import JTS_TYPES, json_default
@@ -11,11 +11,11 @@ class SchemaTable(object):
     """ A SchemaTable uses the given JSON Table Schema to reflect, and, if
     necessary, generate a database table within the specified database. """
 
-    def __init__(self, engine, table_name, schema):
+    def __init__(self, engine, table_name, schema, metadata=MetaData()):
         self.bind = engine
         self.table_name = table_name
         self.schema = schema
-        self.meta = MetaData()
+        self.meta = metadata
         self.meta.bind = self.bind
         self._table = None
 
@@ -41,8 +41,25 @@ class SchemaTable(object):
         specifications. """
         for field in self.schema.get('fields'):
             data_type = JTS_TYPES.get(field.get('type'), Unicode)
-            col = Column(field.get('name'), data_type, nullable=True)
+            foreign_key = self._foreign_key(field)
+            if foreign_key:
+                col = Column(field.get('name'), data_type,
+                             ForeignKey(foreign_key), nullable=True)
+            else:
+                col = Column(field.get('name'), data_type, nullable=True)
             table.append_column(col)
+
+    def _foreign_key(self, field):
+        """ If schema has 'foreignKeys' key, iterate through list of foreign
+        keys until match to current field found and return table/column ref. """
+        fk = None
+        sfks = self.schema.get('foreignKeys')
+        if type(sfks) == list:
+            for sfk in sfks:
+                if sfk['fields'] == field.get('name'):
+                    fk = "{0}.{1}".format(sfk['reference']['resource'],
+                                          sfk['reference']['fields'])
+        return fk
 
     def load_iter(self, iterable, chunk_size=1000):
         """ Bulk load all the data in an artifact to a matching database

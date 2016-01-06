8aa5d16fef4cd9805da7755cd735c277622de230
roll <roll@post.agency>
2016-01-06 14:59:33 +0300

    reimplemented examples/systests system

---
+++ examples/spending.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import sys
+from pprint import pprint
+
+sys.path.insert(0, '.')
+from examples.base import run
+
+
+# Fixtures
+url = 'sqlite:///:memory:'
+prefix = 'test_'
+table = 'test'
+schema = {
+    'fields': [
+        {
+            'name': 'id',
+            'type': 'string',
+        },
+        {
+            'name': 'parent',
+            'type': 'string',
+        },
+        {
+            'name': 'name',
+            'type': 'string',
+        },
+        {
+            'name': 'current',
+            'type': 'boolean',
+        },
+        {
+            'name': 'amount',
+            'type': 'number',
+        },
+    ],
+    'primaryKey': 'id',
+    'foreignKeys': [
+        {
+            'fields': 'parent',
+            'reference': {
+                'resource': 'self',
+                'fields': 'id',
+            },
+        },
+    ]
+}
+data = [
+    ('A3001', 'A3001', 'Taxes', True, 10000.5),
+    ('A5032', 'A3001', 'Parking Fees', False, 2000.5),
+]
+
+
+# Execution
+if __name__ == '__main__':
+    pprint(run(url, prefix, table, schema, data))
---
+++ tests/system/test_storage.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import os
+import io
+import six
+import sys
+import json
+import runpy
+import tempfile
+import unittest
+
+from examples import base
+from examples import spending
+
+
+class TestResource(unittest.TestCase):
+
+    # Tests
+
+    def test_spending(self):
+
+        # Run function
+        tables, schema, data = base.run(
+                spending.url, spending.prefix, spending.table,
+                spending.schema, spending.data)
+
+        # Assert values
+        assert tables == [spending.table]
+        # TODO: return assertions
+        # assert schema == [spending.schema]
+        # assert data == [spending.data]

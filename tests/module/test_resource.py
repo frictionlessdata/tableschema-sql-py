0ca981d29593b92187759ad3c02b05db9b1724ca
roll <roll@post.agency>
2016-01-06 16:39:48 +0300

    implemented import_resource module test

--- tests/module/test_resource.py
+++ tests/module/test_resource.py
@@ -17,14 +17,26 @@ class Test_import_resource(unittest.TestCase):
     # Helpers
 
     def setUp(self):
-        pass
-
+        self.addCleanup(patch.stopall)
+        self.topen = patch.object(module, 'topen').start()
+        self.SchemaModel = patch.object(module, 'SchemaModel').start()
+        self.storage = MagicMock()
 
     # Tests
 
-    @unittest.skip('write')
     def test(self):
-        pass
+
+        # Call function
+        module.import_resource(self.storage, 'table', 'schema', 'data')
+
+        # Assert calls
+        self.SchemaModel.assert_called_with('schema')
+        self.storage.check.assert_called_with('table')
+        self.storage.create.assert_called_with(
+                'table', self.SchemaModel.return_value.as_python)
+        self.topen.assert_called_with('data', with_headers=True)
+        self.storage.write.assert_called_with(
+                'table', self.topen.return_value.__enter__.return_value)
 
 
 class Test_export_resource(unittest.TestCase):
@@ -32,8 +44,10 @@ class Test_export_resource(unittest.TestCase):
     # Helpers
 
     def setUp(self):
-        pass
-
+        self.addCleanup(patch.stopall)
+        self.ensure_dir = patch.object(module, '_ensure_dir').start()
+        self.SchemaModel = patch.object(module, 'SchemaModel').start()
+        self.storage = MagicMock()
 
     # Tests
 

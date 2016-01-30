94da5699dc5dce40c0de5cc4af994f211048d233
roll <roll@post.agency>
2016-01-30 23:35:06 +0300

    moved resource to datapackage-storage-py

--- jtssql/__init__.py
+++ jtssql/__init__.py
@@ -4,5 +4,4 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from .resource import import_resource, export_resource
 from .storage import Storage

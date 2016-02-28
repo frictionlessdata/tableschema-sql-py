f75d2e2857adb6c1539ec409330d643557b3abfc
roll <roll@post.agency>
2016-02-28 21:26:20 +0300

    updated management tools

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,28 +1,48 @@
 # Contributing
 
-This section is intended to be used by tech users collaborating
-on this project.
+The project follows the [Open Knowledge International coding standards](https://github.com/okfn/coding-standards).
 
 ## Getting Started
 
-To activate virtual environment, install
-dependencies, add pre-commit hook to review and test code
-and get `run` command as unified developer interface:
+Recommended way to get started is to create and activate a project virtual environment.
+To install package and development dependencies into active environment:
 
 ```
-$ source activate.sh
+$ make develop
+```
+
+## Linting
+
+To lint the project codebase:
+
+```
+$ make lint
+```
+
+Under the hood `pylint` configured in `pylintrc` is used. On this stage it's already
+installed into your environment and could be used separately with more fine-grained control
+as described in documentation - https://www.pylint.org/.
+
+For example to check only errors:
+
+```
+$ pylint -E <path>
 ```
 
 ## Testing
 
-To run code review and tests with coverage check:
+To run tests with coverage:
 
 ```
-$ run test
+$ make test
 ```
+Under the hood `tox` powered by `py.test` and `coverage` configured in `tox.ini` is used.
+It's already installed into your environment and could be used separately with more fine-grained control
+as described in documentation - https://testrun.org/tox/latest/.
 
-Coverage data will be in the `.coverage` file.
+For example to check subset of tests against Python 2 environment with increased verbosity.
+All positional arguments and options after `--` will be passed to `py.test`:
 
-The project follow the next style guides:
-- [Open Knowledge Coding Standards and Style Guide](https://github.com/okfn/coding-standards)
-- [PEP 8 - Style Guide for Python Code](https://www.python.org/dev/peps/pep-0008/)
+```
+tox -e py27 -- -v tests/<path>
+```
--- setup.py
+++ setup.py
@@ -2,51 +2,72 @@
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
-from __future__ import unicode_literals
 
 import os
 import io
-import json
 from setuptools import setup, find_packages
 
 
 # Helpers
-def read(path):
+def read(*paths):
+    """Read a text file."""
     basedir = os.path.dirname(__file__)
-    return io.open(os.path.join(basedir, path), encoding='utf-8').read()
-def clean(deps):
-    res = []
-    for dep in deps:
-        if dep and not dep.startswith('#') and not dep.startswith('git'):
-            res.append(dep)
-    return res
+    fullpath = os.path.join(basedir, *paths)
+    contents = io.open(fullpath, encoding='utf-8').read().strip()
+    return contents
 
 
 # Prepare
-readme = read('README.md')
-license = read('LICENSE.txt')
-requirements = clean(read('requirements.txt').split())
-requirements_dev = clean(read('requirements.dev.txt').split())
-package = json.loads(read('package.json'))
+PACKAGE = 'jtssql'
+INSTALL_REQUIRES = [
+    'six',
+    'sqlalchemy',
+    'jsontableschema',
+    'tabulator',
+]
+LINT_REQUIRES = [
+    'pylint',
+]
+TESTS_REQUIRE = [
+    'tox',
+]
+README = read('README.md')
+VERSION = read(PACKAGE, 'VERSION')
+PACKAGES = find_packages(exclude=['examples', 'tests'])
 
 
 # Run
 setup(
-    name=package['name'],
-    version=package['version'],
-    description=package['description'],
-    long_description=readme,
-    author=package['author'],
-    author_email=package['author_email'],
-    url=package['repository'],
-    license=package['license'],
+    name=PACKAGE,
+    version=VERSION,
+    packages=PACKAGES,
     include_package_data=True,
-    packages=find_packages(exclude=['examples', 'tests']),
-    package_dir={package['slug']: package['slug']},
-    install_requires=requirements,
-    tests_require=requirements_dev,
-    test_suite='nose.collector',
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require={'develop': LINT_REQUIRES + TESTS_REQUIRE},
     zip_safe=False,
-    keywords=package['keywords'],
-    classifiers=package['classifiers'],
+    long_description=README,
+    description='Generate SQL tables, load and extract data, based on JSON Table Schema descriptors.',
+    author='Open Knowledge Foundation',
+    author_email='info@okfn.org',
+    url='https://github.com/okfn/jsontableschema-sql-py',
+    license='MIT',
+    keywords=[
+        'frictionless data',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
 )
